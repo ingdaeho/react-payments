360b0cb716025a0097fe8156e27775b2d9d2d4f6
ingdaeho <ingdaeho@swingby.us>
2024-05-08 13:33:34 +0900

    feat: Button add disabled props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,16 +3,22 @@ import classNames from 'classnames';
 
 interface Props extends ComponentPropsWithoutRef<'span'> {
   variant?: 'text' | 'contained' | 'outlined';
+  disabled?: boolean;
 }
 
 const Button = ({
   variant = 'text',
+  disabled = false,
   className,
   children,
   ...props
 }: PropsWithChildren<Props>) => {
   return (
-    <button className={classNames(`button-${variant}`, className)} {...props}>
+    <button
+      className={classNames(`button-${variant}`, className, { disabled })}
+      disabled={disabled}
+      {...props}
+    >
       {children}
     </button>
   );
